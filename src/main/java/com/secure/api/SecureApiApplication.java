ddd6ab4dab1f73a01b7dfd69bc1d742e1d78ce63
mohammad.semnani <46643886+cloud-4j@users.noreply.github.com>
2023-02-11 23:32:15 +0100

    Add User and Role

--- src/main/java/com/secure/api/SecureApiApplication.java
+++ src/main/java/com/secure/api/SecureApiApplication.java
@@ -1,19 +1,7 @@
 package com.secure.api;
 
-import com.secure.api.entity.RoleEntity;
-import com.secure.api.entity.UserEntity;
-import com.secure.api.service.RoleService;
-import com.secure.api.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecureApiApplication {
